f377fa9c7b73a299523e7f73433e7b3f0dc9adb7
Dara Adekore <dara@Daras-MacBook-Pro.local>
2025-02-17 15:01:06 -0800

    styling for responsiveness

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import SearchBarWithRadius from './SearchBarWithRadius';
 import { Logout } from './Logout';
+import { PulsatingLogoSmallWhite } from '../Logo/PulsatingLogoSmallWhite';
+import { useAppSelector } from '../../store/hooks';
 import '../../styles/Navbar.css';
 
 const Navbar = () => {
+  const user = useAppSelector((state) => state.user);
+  
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-brand">
-          <img src="/logo.png" alt="Yapper" className="navbar-logo" />
+          <PulsatingLogoSmallWhite />
         </div>
         <div className="navbar-search">
           <SearchBarWithRadius />
         </div>
         <div className="navbar-actions">
+          <span className="username">{user.username}</span>
           <Logout />
         </div>
       </div>
